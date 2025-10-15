7c112f0aefa9937670c38938567d949c74efe46e
mohmdsaalim <muhammedsaalim005@gmail.com>
2025-10-15 09:29:57 +0530

    Time: 1 ms (89.51%), Space: 58.6 MB (9.62%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.js
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.js
@@ -2,16 +2,16 @@ var myAtoi = function(str) {
     let i = 0, sign = 1, num = 0;
     const MAX = 2147483647, MIN = -2147483648;
 
-    // skip spaces
+
     while (i < str.length && str[i] === ' ') i++;
 
-    // sign check
+
     if (str[i] === '-' || str[i] === '+') {
         sign = str[i] === '-' ? -1 : 1;
         i++;
     }
 
-    // build number
+
     while (i < str.length && str[i] >= '0' && str[i] <= '9') {
         num = num * 10 + (str[i].charCodeAt(0) - 48);
 
