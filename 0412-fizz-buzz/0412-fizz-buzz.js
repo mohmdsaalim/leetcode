45f8f66ee504781fc1fc498d105579e7436f6de5
mohmdsaalim <muhammedsaalim005@gmail.com>
2025-10-17 18:13:55 +0530

    Time: 0 ms (100%), Space: 58.7 MB (26.22%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function(n) {
+    let result = [];
+
+    for (let a = 1; a <= n; a++) {
+        if (a % 3 === 0 && a % 5 === 0) {
+            result.push("FizzBuzz");
+        } else if (a % 3 === 0) {
+            result.push("Fizz");
+        } else if (a % 5 === 0) {
+            result.push("Buzz");
+        } else {
+            result.push(`${a}`);
+        }
+    }
+
+    return result;
+};
+
+console.log(fizzBuzz(15));
