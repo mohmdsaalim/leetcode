e1b95da0c15a389062ce4ab3411433df97d8765d
Mohammed Saalim  K m <muhammedsaalim005@gmail.com>
2025-10-21 09:31:32 +0530

    Time: 52 ms (80.54%), Space: 58.6 MB (65.73%) - LeetHub

---
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+  let i = 0; 
+
+  for (let n of nums) {
+    
+    if (i < 2 || n !== nums[i - 2]) {
+      nums[i] = n;
+      i++;
+    }
+  }
+  
+  return i; 
+};
