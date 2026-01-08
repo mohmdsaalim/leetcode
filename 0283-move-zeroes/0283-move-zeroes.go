ced48894bbc8e3d91e8c40eea15895f230e6fda9
Saalim <muhammedsaalim005@gmail.com>
2026-01-08 18:02:50 +0530

    Time: 0 ms (100%), Space: 8.3 MB (97.78%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.go
@@ -0,0 +1,10 @@
+func moveZeroes(nums []int)  {
+    l := 0
+    for r := 0; r < len(nums);r++{
+        if nums[r] != 0 {
+            nums[l], nums[r] = nums[r], nums[l]
+            l++
+        }
+    }
+
+}
