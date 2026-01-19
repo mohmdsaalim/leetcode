2124cf3e3cb1ae3d6977b05709bcf79d449c2402
Saalim <muhammedsaalim005@gmail.com>
2026-01-19 21:55:06 +0530

    Time: 0 ms (100%), Space: 4.1 MB (31.05%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.go
@@ -0,0 +1,24 @@
+func mySqrt(x int) int {
+	if x < 2 {
+		return x
+	}
+
+	left, right := 1, x/2
+	ans := 0
+
+	for left <= right {
+		mid := left + (right-left)/2
+		if mid*mid == x {
+			return mid
+		}
+
+		if mid*mid < x {
+			ans = mid
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+
+	return ans
+}
