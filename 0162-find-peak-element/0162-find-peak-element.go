e69c23b0f9ff3ab9a66885054e0468ca2740bb2c
Saalim <muhammedsaalim005@gmail.com>
2026-02-02 20:39:05 +0530

    Time: 0 ms (100%), Space: 4.5 MB (71.93%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.go
+++ 0162-find-peak-element/0162-find-peak-element.go
@@ -1,10 +1,12 @@
 func findPeakElement(nums []int) int {
-    var maxIndex int
+	max := nums[0]
+	maxIdx := 0
 
-    for i := 0; i < len(nums)-1; i++ {
-		if nums[i] < nums[i+1] {
-			maxIndex = i + 1
+	for i := 1; i < len(nums); i++ {
+		if nums[i] > max {
+			max = nums[i]
+			maxIdx = i
 		}
 	}
-	return maxIndex
+	return maxIdx
 }
