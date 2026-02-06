c5ff23d2e6b63f82dcf06bb86ffdd6ba00bf24cc
Saalim <muhammedsaalim005@gmail.com>
2026-02-06 20:13:48 +0530

    Time: 0 ms (100%), Space: 5 MB (66.52%) - LeetHub

---
+++ 0120-triangle/0120-triangle.go
@@ -0,0 +1,14 @@
+func minimumTotal(triangle [][]int) int {
+    for row := len(triangle) - 2; row >= 0; row-- {
+        for col := 0; col <= row; col++ {
+            belowLeft := triangle[row+1][col]
+            belowRight := triangle[row+1][col+1]
+            triangle[row][col] += min(belowLeft, belowRight)
+        }
+    }
+    return triangle[0][0]
+}
+func min(a, b int) int {
+    if a < b { return a }
+    return b
+}
