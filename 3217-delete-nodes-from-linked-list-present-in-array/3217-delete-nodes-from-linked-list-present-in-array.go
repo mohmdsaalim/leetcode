e4e7bcbd6f01748e8468357aae91108ab67a9847
Saalim <muhammedsaalim005@gmail.com>
2026-02-07 16:36:05 +0530

    Time: 68 ms (10.2%), Space: 13.6 MB (83.67%) - LeetHub

--- 3217-delete-nodes-from-linked-list-present-in-array/3217-delete-nodes-from-linked-list-present-in-array.go
+++ 3217-delete-nodes-from-linked-list-present-in-array/3217-delete-nodes-from-linked-list-present-in-array.go
@@ -5,27 +5,48 @@
  *     Next *ListNode
  * }
  */
+// func modifiedList(nums []int, head *ListNode) *ListNode {
+//     if head == nil{
+//         return nil
+//     }
+
+//     valueSet := make(map[int]bool)
+//     for _, value := range nums{
+//         valueSet[value] = true
+//     }
+//     dummy := &ListNode{}
+//     dummy.Next = head
+//     previousNode := dummy
+//     currentNode := head
+
+//     for currentNode != nil{
+//         if valueSet[currentNode.Val] {
+//           previousNode.Next = currentNode.Next
+//         }else{
+//                  previousNode = currentNode
+//         }
+//         currentNode = currentNode.Next
+//     }
+//     return dummy.Next
+// }
+
 func modifiedList(nums []int, head *ListNode) *ListNode {
-    if head == nil{
-        return nil
+    valueSet := make(map[int]struct{})
+    for _, v := range nums {
+        valueSet[v] = struct{}{}
     }
 
-    valueSet := make(map[int]bool)
-    for _, value := range nums{
-        valueSet[value] = true
-    }
-    dummy := &ListNode{}
-    dummy.Next = head
-    previousNode := dummy
-    currentNode := head
+    dummy := &ListNode{Next: head}
+    prev, curr := dummy, head
 
-    for currentNode != nil{
-        if valueSet[currentNode.Val] {
-          previousNode.Next = currentNode.Next
-        }else{
-                 previousNode = currentNode
+    for curr != nil {
+        if _, exists := valueSet[curr.Val]; exists {
+            prev.Next = curr.Next
+        } else {
+            prev = curr
         }
-        currentNode = currentNode.Next
+        curr = curr.Next
     }
+
     return dummy.Next
 }
