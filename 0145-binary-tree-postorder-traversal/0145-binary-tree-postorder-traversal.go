8b17bed0eef8a473e5bc68fec9abd9b1cdcab8fa
Saalim <muhammedsaalim005@gmail.com>
2026-03-10 14:59:09 +0530

    Time: 0 ms (100%), Space: 4.1 MB (31.53%) - LeetHub

---
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.go
@@ -0,0 +1,15 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func postorderTraversal(root *TreeNode) []int {
+    if root == nil{
+        return []int{}
+    }
+
+    return append(append(postorderTraversal(root.Left), postorderTraversal(root.Right)...), root.Val)
+}
